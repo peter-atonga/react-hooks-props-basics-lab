905e4b9b4bb1bdc374785485507009312dc471eb
peter atonga <peteratonga@gmail.com>
2024-08-01 15:54:19 +0300

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,29 @@
 import React from "react";
+import user from "../data/user";
+import Links from "./Links";
+function About(user) {
+  // if(!user.bio){
+  //   null
+  // }else{
+  //   return (
+  //     <div id="about">
+  //       <h2>About Me</h2>
+  //       <p>{user.bio}</p> 
+  //       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+  //       {/* add your <Links /> component here */}
+  //     </div>
+  //   );
+  // }
 
-function About() {
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {user.bio ? <p>{user.bio}</p> : null} 
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links />
+
     </div>
   );
 }
